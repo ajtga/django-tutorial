882c7e3a3f5442a1433b9cf9f30686de4bc2ff90
Augusto José <ajtgalencar@gmail.com>
2018-02-21 10:41:20 -0300

    Update index view to a class based generic view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from django.db.models import F
 # from django.http import Http404
 # from django.template import loader
@@ -8,14 +9,13 @@ from django.db.models import F
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-publication_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-publication_date')[:5]
 
 
 def detail(request, question_id):
